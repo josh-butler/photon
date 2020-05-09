1f75baede01853092ba2a7bc793c3df42dcdb52a
Josh Butler <scinewave@gmail.com>
2020-05-09 16:02:28 -0400

    add npm pkg

--- src/functions/hello.ts
+++ src/functions/hello.ts
@@ -1,14 +1,14 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import { has } from 'lodash';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const handler: APIGatewayProxyHandler = async (event, _context) => {
-  const { httpMethod } = event;
-  console.log('httpMethod: ', httpMethod);
+  const msg = has(event, 'httpMethod');
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go!! Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      message: `Go!!${msg} Serverless Webpack (Typescript) v1.0! Your function executed successfully!`,
       input: event,
     }, null, 2),
   };
