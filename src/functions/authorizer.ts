5ec3b725a1ff6d7ea23cf70903d18f80020f31b9
Josh Butler <scinewave@gmail.com>
2020-05-17 16:45:07 -0400

    Update authorizer

--- src/functions/authorizer.ts
+++ src/functions/authorizer.ts
@@ -1,13 +1,42 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
-// import { has } from 'lodash';
-// import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
+
+const buildIAMPolicy = (userId, effect, resource, context) => {
+  console.log(`buildIAMPolicy ${userId} ${effect} ${resource}`);
+  const policy = {
+    principalId: userId,
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: effect,
+          Resource: resource,
+        },
+      ],
+    },
+    context,
+  };
+  return policy;
+};
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const handler: APIGatewayProxyHandler = async (event, _context) => {
-  console.log('event: ', event);
-  return {
-    statusCode: 200,
-    body: 'okok',
-  };
+export const handler = async (event, _context) => {
+  const { authorizationToken, methodArn } = event;
+  try {
+    // Verify JWT
+    const decoded = jwt.verify(authorizationToken, process.env.JWT_SECRET);
+    const { role, user } = decoded;
+    const isAllowed = role === 'admin';
+    const effect = isAllowed ? 'Allow' : 'Deny';
+
+    // Return an IAM policy document for the current endpoint
+    const userId = user.username;
+    const authorizerContext = { user: JSON.stringify(user) };
+    const policyDocument = buildIAMPolicy(userId, effect, methodArn, authorizerContext);
+    return policyDocument;
+  } catch (err) {
+    console.log(err.message);
+    throw new Error('Unauthorized');
+  }
 };
