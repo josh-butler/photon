1ecf1b22275cc5ebd757c7049d6a67ffc61502ef
Josh Butler <scinewave@gmail.com>
2020-06-17 20:37:49 -0400

    Util stuff

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,3 +1,26 @@
+import { groupBy, range, isEqual } from 'lodash';
+// import { randomBytes } from 'crypto';
+// .range(1, 5);
+
+const validRounds = rounds => {
+  const numbers = Object.keys(rounds).map(n => parseInt(n, 10));
+  return isEqual(numbers, range(1, numbers.length + 1));
+};
+
+const validPositions = rounds => {
+  const results = Object.entries(rounds).map(rnd => {
+    const positions: any = rnd[1];
+    const pos = positions.map(r => r.selectNum);
+    return isEqual(pos, range(pos.length));
+  });
+  return results.every(i => i);
+};
+
+const validatePickPositions = (picks: any[]) => {
+  const rounds = groupBy(picks, 'round');
+  return validRounds(rounds) && validPositions(rounds);
+};
+
 const getName = () => 'name';
 
-export { getName };
+export { getName, validatePickPositions };
--- test/util.test.ts
+++ test/util.test.ts
@@ -1,6 +1,27 @@
-import { getName } from '../src/util/util';
+import { getName, validatePickPositions } from '../src/util/util';
 
 test('returns-correct-name', () => {
   const name = getName();
   expect(name).toEqual('name');
+});
+
+describe('util-order', () => {
+  it('should-detect-duplicate-positons-in-round', () => {
+    const unique = [
+      { round: 2, selectNum: 0 },
+      { round: 2, selectNum: 1 },
+      { round: 1, selectNum: 0 },
+      { round: 1, selectNum: 1 },
+      { round: 1, selectNum: 2 },
+    ];
+
+    const duplicate = [
+      { round: 1, selectNum: 0 },
+      { round: 1, selectNum: 1 },
+      { round: 1, selectNum: 1 },
+    ];
+
+    expect(validatePickPositions(unique)).toBe(true);
+    expect(validatePickPositions(duplicate)).toBe(false);
+  });
 });
